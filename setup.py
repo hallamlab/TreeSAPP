8ce278d02aea42d431d52eb4d1ff10f5a9cb7f57
cmorganl <c.morganlang@gmail.com>
2019-02-04 17:48:36 -0800

    New setup in prep for pypi upload

--- setup.py
+++ setup.py
@@ -1,11 +1,46 @@
-from distutils.core import setup, Extension
+from setuptools import Extension
+from setuptools import setup, find_packages
 
-setup(name='TreeSAPP_extend',
-      version='0.1',
-      description='Parsing utilities for TreeSAPP',
-      author='Connor Morgan-Lang',
-      author_email='c.morganlang@gmail.com',
-      ext_modules=[Extension("_tree_parser", ["sub_binaries/TreeSAPP_extensions/tree_parsermodule.cpp"]),
-                   Extension("_fasta_reader", ["sub_binaries/TreeSAPP_extensions/fasta_reader.cpp"])],
-      requires=['pygtrie', 'ete3', 'numpy']
-      )
+with open("README.md", "r") as readme:
+    LONG_DESCRIPTION = readme.read()
+
+CLASSIFIERS = [
+    "Environment :: Console",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Natural Language :: English",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: MacOS :: MacOS X",
+    "Programming Language :: C++",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+]
+
+
+SETUP_METADATA = \
+               {
+                     "name": "TreeSAPP-cmorganl",
+                     "version": "0.2.0",
+                     "description": "TreeSAPP is a functional and taxonomic annotation tool",
+                     "long_description": LONG_DESCRIPTION,
+                     "long_description_content_type": "text/markdown",
+                     "author": "Connor Morgan-Lang",
+                     "author_email": "c.morganlang@gmail.com",
+                     "url": "https://github.com/hallamlab/TreeSAPP",
+                     "license": "GPL-3.0",
+                     "packages": find_packages(),
+                     "classifiers": CLASSIFIERS,
+                     "ext_modules": [Extension("_tree_parser",
+                                               sources=["sub_binaries/TreeSAPP_extensions/tree_parsermodule.cpp"],
+                                               language="c++",
+                                               include_dirs=["./sub_binaries/TreeSAPP_extensions"]),
+                                     Extension("_fasta_reader",
+                                               sources=["sub_binaries/TreeSAPP_extensions/fasta_reader.cpp"],
+                                               language="c++",
+                                               include_dirs=["./sub_binaries/TreeSAPP_extensions"])
+                                     ],
+                     "install_requires": ["pygtrie", "ete3>=2.3", "numpy", "biopython>=1.68"]
+               }
+
+setup(**SETUP_METADATA)
