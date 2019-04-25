c45158afaf5f7ab29295b6dbd826aaa7a79fd61b
gracez72 <gracezhang72@gmail.com>
2019-04-25 23:51:47 +0000

    edit java installation

--- terraform/tf/scripts/user_data.sh
+++ terraform/tf/scripts/user_data.sh
@@ -17,18 +17,33 @@ sudo apt-get install python3-pip -y
 which pip3
 ln -s /usr/bin/pip3 /usr/bin/pip 
 
+#Java dependency
+sudo apt-get install libc6-i386 -y
+sudo mkdir ~/Downloads/
+cd ~/Downloads/
+sudo wget http://storage.googleapis.com/travis-java/jdk-8u211-linux-i586.tar.gz
+if [ -d "/usr/lib/jvm" ]; then rm -rf /usr/lib/jvm; fi
+sudo mkdir /usr/lib/jvm
+cd /usr/lib/jvm
+sudo tar -xvzf ~/Downloads/jdk-8u211-linux-i586.tar.gz
+
+echo "PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/jdk1.8.0_211/bin:/usr/lib/jvm/jdk1.8.0_211/db/bin:/usr/lib/jvm/jdk1.8.0_211/jre/bin\"
+J2SDKDIR=\"/usr/lib/jvm/jdk1.8.0_211\"
+J2REDIR=\"/usr/lib/jvm/jdk1.8.0_211/jre*\"
+JAVA_HOME=\"/usr/lib/jvm/jdk1.8.0_211\"
+DERBY_HOME=\"/usr/lib/jvm/jdk1.8.0_211/db\"" > /etc/environment
+
+source /etc/environment
+
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_211/bin/java" 0
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_211/bin/javac" 0
+sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_211/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/jdk1.8.0_211/bin/javac
+
 #Miscellaneous dependencies
 sudo pip install numpy biopython scipy 
 sudo apt-get install python-qt4 python-lxml python-six -y
 sudo pip install --upgrade ete3 
-
-#Java dependency
-sudo apt-get update
-sudo apt-get install -y software-properties-common debconf-utils
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-sudo echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-sudo apt-get install -y oracle-java8-installer oracle-java8-set-default 
 
 GITHUB_PATH="/root/github"
 
