0551ec8d793b483a9cf2da1e141f73b0879216f0
Connor Morgan-Lang <c.morganlang@gmail.com>
2019-11-27 16:29:27 -0800

    Version 0.6.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ CLASSIFIERS = [
 SETUP_METADATA = \
     {
         "name": "treesapp",
-        "version": "0.6.1",
+        "version": "0.6.3",
         "description": "TreeSAPP is a functional and taxonomic annotation tool",
         "long_description": LONG_DESCRIPTION,
         "long_description_content_type": "text/markdown",
--- treesapp/__init__.py
+++ treesapp/__init__.py
@@ -1,4 +1,4 @@
 #! /usr/bin/env python
 
 name = "treesapp"
-version = "0.6.1"
+version = "0.6.3"
