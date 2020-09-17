72434a819b514c6ff4ba0915d54b34019ab41207
cmorganl <c.morganlang@gmail.com>
2020-09-17 13:20:30 -0700

    Remove unused variables

--- tests/test_fasta.py
+++ tests/test_fasta.py
@@ -14,8 +14,8 @@ class MyTestCase(unittest.TestCase):
 
     def tearDown(self) -> None:
         from glob import glob
-        test_fa_prefix, ext = os.path.splitext(os.path.basename(self.test_fa))
-        test_fq_prefix, ext = os.path.splitext(os.path.basename(self.test_fq))
+        test_fa_prefix, _ = os.path.splitext(os.path.basename(self.test_fa))
+        test_fq_prefix, _ = os.path.splitext(os.path.basename(self.test_fq))
         for file_name in sorted(glob(test_fa_prefix + "*") + glob(test_fq_prefix + "*")):
             if os.path.isfile(file_name):
                 os.remove(file_name)
@@ -35,23 +35,23 @@ class MyTestCase(unittest.TestCase):
         from treesapp.fasta import sequence_info_groups
         # Test one: unformatted
         seq_name = "650716056_50856936 Metig_1237"
-        header_format_re, header_db, header_molecule = get_header_format(seq_name, self.header_regexes)
+        header_format_re, header_db, _ = get_header_format(seq_name, self.header_regexes)
         seq_info_tuple = sequence_info_groups(header_format_re.match(seq_name), header_db, seq_name)
         self.assertEqual("650716056_50856936", seq_info_tuple.accession)
         # Test two: NCBI protein
         seq_name = ">AKB49151.1 Methyl coenzyme M reductase alpha subunit [Methanosarcina sp. Kolksee]"
-        header_format_re, header_db, header_molecule = get_header_format(seq_name, self.header_regexes)
+        header_format_re, header_db, _ = get_header_format(seq_name, self.header_regexes)
         seq_info_tuple = sequence_info_groups(header_format_re.match(seq_name), header_db, seq_name)
         self.assertEqual("AKB49151", seq_info_tuple.accession)
         # Test three: EggNOG
         seq_name = ">1000565.METUNv1_03969"
-        header_format_re, header_db, header_molecule = get_header_format(seq_name, self.header_regexes)
+        header_format_re, header_db, _ = get_header_format(seq_name, self.header_regexes)
         seq_info_tuple = sequence_info_groups(header_format_re.match(seq_name), header_db, seq_name)
         self.assertEqual("1000565.METUNv1_03969", seq_info_tuple.accession)
         self.assertEqual("1000565", seq_info_tuple.taxid)
         # Test four: SwissProt, representing accessions with database prefix (e.g. 'sp') with accession enclosed by '|'
         seq_name = 'sp|P06008|RCEH_BLAVI Reaction center protein H chain OS=Blastochloris viridis GN=puhA'
-        header_format_re, header_db, header_molecule = get_header_format(seq_name, self.header_regexes)
+        header_format_re, header_db, _ = get_header_format(seq_name, self.header_regexes)
         self.assertEqual("sp", header_db)
         seq_info_tuple = sequence_info_groups(header_format_re.match(seq_name), header_db, seq_name)
         self.assertEqual("P06008", seq_info_tuple.accession)
@@ -60,7 +60,7 @@ class MyTestCase(unittest.TestCase):
     def test_sequence_info_groups_assigned(self):
         from treesapp.fasta import get_header_format, sequence_info_groups
         seq_name = 'sp|P06008|RCEH_BLAVI Reaction center protein H chain OS=Blastochloris viridis|PuhA|1_258'
-        header_format_re, header_db, header_molecule = get_header_format(seq_name, self.header_regexes)
+        header_format_re, header_db, _ = get_header_format(seq_name, self.header_regexes)
         self.assertEqual("ts_assign", header_db)
         seq_info_tuple = sequence_info_groups(header_format_re.match(seq_name), header_db, seq_name, self.header_regexes)
         self.assertEqual("P06008", seq_info_tuple.accession)
