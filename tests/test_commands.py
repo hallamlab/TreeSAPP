4058502c11b2cc1f27a9430e128c435ec2a78fb4
cmorganl <c.morganlang@gmail.com>
2021-01-06 10:57:10 +0100

    GitHub Action YAMLs, prepare for release

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -52,7 +52,7 @@ class TreesappTester(unittest.TestCase):
         self.assertEqual(len(pre_lines), len(post_lines))
         # Ensure the name of the sample is substituted for the sample ID
         self.assertEqual({"test_TarA.1"}, set([line[0] for line in post_lines]))
-        self.assertEqual(1773, round(sum(abund_dict.values())))
+        self.assertEqual(148, round(sum(abund_dict.values())))
         # Replace the classification table
         copyfile(os.path.join(self.ts_assign_output, "tmp.tsv"), classification_table)
         os.remove(os.path.join(self.ts_assign_output, "tmp.tsv"))
--- treesapp/__init__.py
+++ treesapp/__init__.py
@@ -1,4 +1,4 @@
 #! /usr/bin/env python
 """A Python package for gene-centric taxonomic and functional classification using phylogenetic placement"""
 name = "treesapp"
-__version__ = "0.9.6"
+__version__ = "0.9.7"
