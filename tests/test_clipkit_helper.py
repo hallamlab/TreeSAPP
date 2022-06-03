5f347729d94996d6e4cf03b49342970615d74319
cmorganl <c.morganlang@gmail.com>
2022-06-03 09:52:55 +0200

    Implemented basic helper class for clipkit and tests

---
+++ tests/test_clipkit_helper.py
@@ -0,0 +1,36 @@
+import os
+import unittest
+
+from .testing_utils import get_test_data
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_fa = get_test_data('PuhA.mfa')
+        self.output_fa = 'PuhA.clipped.mfa'
+
+    def tearDown(self) -> None:
+        if os.path.isfile(self.output_fa):
+            os.remove(self.output_fa)
+
+    def test_run(self):
+        from treesapp import clipkit_helper
+        from clipkit import modes as ck_modes
+        ck = clipkit_helper.ClipKitHelper(fasta_in=self.test_fa,
+                                          mfa_out=self.output_fa,
+                                          mode="smart-gap")
+        ck.run()
+        self.assertTrue(os.path.isfile(self.output_fa))
+
+        ck.mode = ck_modes.TrimmingMode("kpi-smart-gap")
+        ck.run()
+        self.assertTrue(os.path.isfile(self.output_fa))
+
+        ck.mode = ck_modes.TrimmingMode("kpi")
+        ck.run()
+        self.assertTrue(os.path.isfile(self.output_fa))
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
--- treesapp/clipkit_helper.py
+++ treesapp/clipkit_helper.py
@@ -1,6 +1,8 @@
 import logging
-from clipkit import clipkit
-from clipkit import args_processing
+import os.path
+
+from clipkit import clipkit as ck
+from clipkit import modes as ck_modes
 
 from treesapp import logger
 
@@ -8,14 +10,33 @@ from treesapp import logger
 class ClipKitHelper:
     CLIPKIT_MODES = {"smart-gap"}
 
-    def __init__(self, fasta_in: str, mfa_out: str):
-        self.logger = logging.getLogger(logger.logger_name())
-        self.input = ""
-        self.mfa_out = ""
+    def __init__(self, fasta_in: str, mfa_out=None, mode="smart-gap", gap_prop=0.9):
+        self.input = fasta_in
+        if mfa_out is None:
+            prefix, ext = os.path.splitext(fasta_in)
+            self.mfa_out = prefix + ".trim" + ext
+        else:
+            self.mfa_out = mfa_out
 
-        self.mode = "smart-gap"
+        self.logger = logging.getLogger(logger.logger_name())
+        self.mode = ck_modes.TrimmingMode(mode)
+        self.gap_prop = gap_prop
+
+        self.ff_in = "fasta"
+        self.ff_out = "fasta"
         return
 
     def run(self):
-        # clipkit.execute()
+
+        ck.execute(input_file=self.input,
+                   input_file_format=self.ff_in,
+                   output_file=self.mfa_out,
+                   output_file_format=self.ff_out,
+                   gaps=self.gap_prop,
+                   complement=False,
+                   mode=self.mode,
+                   use_log=False)
+        return
+
+    def summarise_trimming(self):
         return
